024d814a389e114619ed1474d38fd38dff161a02
Ayush-Asutkar <asutkarayush@gmail.com>
2022-08-21 14:27:14 +0530

    Assignment 1: React Components, added DishDetailComponent

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import DishDetail from "../shared/DishDetailComponent";
 
 class Menu extends Component {
 
@@ -9,6 +10,11 @@ class Menu extends Component {
         this.state = {
             selectedDish: null
         }
+        console.log('Menu Component constructor is invoked');
+    }
+
+    componentDidMount() {
+        console.log('Menu Component componentDidMount is invoked');
     }
 
     onDishSelect(dish) {
@@ -48,16 +54,15 @@ class Menu extends Component {
             );
         });
 
+        
+        console.log('Menu Component render is invoked');
+
         return (
             <div className="container">
                 <div className="row">
                     {menu}
                 </div>
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderDish(this.state.selectedDish)}
-                    </div>
-                </div>
+                <DishDetail dish={this.state.selectedDish} />
             </div>
         );
     }
